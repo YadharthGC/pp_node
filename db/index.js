90de621fdb499eb4da9e6cf127091ce5dd30ea5d
YadharthGC <ganeshyadharth@gmail.com>
2022-01-10 10:31:28 +0530

    initial

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const mongodb = require("mongodb");
 
 mongoose.connect(
-  "mongodb+srv://ganesh:chitra@cluster0.2pjhw.mongodb.net/URL?retryWrites=true&w=majority",
+  "mongodb+srv://ganesh:chitra@cluster0.2pjhw.mongodb.net/PP?retryWrites=true&w=majority",
   (err) => {
     if (err) return console.log(error);
     console.log("connected to mongodb");
